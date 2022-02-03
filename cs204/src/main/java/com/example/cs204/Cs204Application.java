5b089d15ece5f552a41e9787404609677b500614
Zhakyp01 <Zhakypzhoomartuulu@gmail.com>
2022-02-03 15:25:19 +0600

    inital commit

---
+++ cs204/src/main/java/com/example/cs204/Cs204Application.java
@@ -0,0 +1,21 @@
+package com.example.cs204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Cs204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs204Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "<h1>Hello World</h1>";
+	}
+
+}
---
+++ cs204/src/test/java/com/example/cs204/Cs204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
